396e63d4d7623b85c5bbbebec477ec427b04c422
Alberto <cocoglobalmedia@icloud.com>
2024-01-14 22:45:17 +0100

    feat: hero done

--- src/services/movieServices.ts
+++ src/services/movieServices.ts
@@ -1,1 +1,16 @@
 const key = import.meta.env.VITE_TMDB_KEY
+const baseUrl = 'https://api.themoviedb.org/3'
+
+const ENDPOINTS = {
+  popular: `${baseUrl}/movie/popular?api_key=${key}`,
+  topRated: `${baseUrl}/movie/top_rated?api_key=${key}`,
+  trending: `${baseUrl}/trending/all/day?api_key=${key}`,
+  comedy: `${baseUrl}/search/movie?api_key=${key}&language=en-US&query=comedy&page=1&include_adult=false`,
+  upcoming: `${baseUrl}/movie/upcoming?api_key=${key}`
+}
+
+export function createImageUrl(filename: string, size: string) {
+  return `https://image.tmdb.org/t/p/${size}/${filename}`
+}
+
+export default ENDPOINTS;
