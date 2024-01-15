486fbb3a5b8b113de8466dc8ddfaf90d44f80cdd
Alberto <cocoglobalmedia@icloud.com>
2024-01-15 23:08:25 +0100

    feat: adding like icon

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,4 +1,9 @@
+import { useState } from 'react';
 import { createImageUrl } from '../services/movieServices'
+import { FaHeart } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
+
+
 
 interface Movie {
   title: string;
@@ -7,12 +12,15 @@ interface Movie {
 }
 
 const MovieItem: React.FC<{ movie: Movie }> = ({ movie }) => {
+  const [like, setLike] = useState(false)
   const { title, backdrop_path, poster_path } = movie
   return (
     <div className='relative w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block rounded-lg overflow-hidden cursor-pointer m-2'>
       <img className='w-full h-40 block object-cover object-top' src={createImageUrl(backdrop_path ?? poster_path, "w500")} alt={title} />
       <div className='absolute top-0 left-0 w-full h-40 bg-black/80 opacity-0 hover:opacity-100 transition'>
         <p className='whitespace-normal text-xs md:text-sm flex justify-center items-center h-full font-nsans-bold'>{movie.title}</p>
+
+        <p>{like ? <FaHeart className="absolute top-2 left-2 text-gray-300" /> : <FaRegHeart className="absolute top-2 left-2 text-gray-300" />}</p>
       </div>
     </div>
   )
