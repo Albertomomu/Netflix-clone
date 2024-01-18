dcb32f7f278f66a06730525bb93eb45a083eca8a
Alberto <cocoglobalmedia@icloud.com>
2024-01-18 22:47:59 +0100

    feat: advancing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,35 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from '../context/AuthContext'
 
 const Navbar = () => {
+
+  const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
+
   return (
     <div className='absolute w-full p-4 flex items-center justify-between z-50'>
       <Link to='/'>
         <h1 className='uppercase text-red-600 font-nsans-bold cursor-pointer text-5xl'>Netflix</h1>
       </Link>
-      <div>
-        <Link to='/login'>
-          <button className='capitalize pr-4'>login</button>
-        </Link>
-        <Link to='/signup'>
-          <button className='capitalize bg-red-600 px-6 py-2 rounded'>sign up</button>
-        </Link>
-      </div>
+      {user?.email ? (
+        <div>
+          <Link to='/profile'>
+            <button className='capitalize pr-4'>profile</button>
+          </Link>
+
+          <button className='capitalize bg-red-600 px-6 py-2 rounded' onClick={logOut()}>logout</button>
+        </div>
+      ) : (
+        <div>
+          <Link to='/login'>
+            <button className='capitalize pr-4'>login</button>
+          </Link>
+          <Link to='/signup'>
+            <button className='capitalize bg-red-600 px-6 py-2 rounded'>sign up</button>
+          </Link>
+        </div>
+      )
+      }
     </div>
   )
 }
