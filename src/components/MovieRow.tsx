8c58c63c054dcb35810d163e52afd0ea9924e9cb
Alberto <cocoglobalmedia@icloud.com>
2024-01-21 22:50:28 +0100

    feat: deleteLike working

--- src/components/MovieRow.tsx
+++ src/components/MovieRow.tsx
@@ -4,6 +4,7 @@ import MovieItem from './MovieItem'
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
 
 const MovieRow = ({ title, url }: { title: string, url: string }) => {
+  const rowId = Math.floor(Math.random() * 1000);
   const [movies, setMovies] = useState<{ id: number, title: string, url: string, backdrop_path: string, poster_path: string, release_date: string, vote_average: number, overview: string, genre_ids: number[] }[]>([])
 
   useEffect(() => {
@@ -11,7 +12,7 @@ const MovieRow = ({ title, url }: { title: string, url: string }) => {
   }, [url])
 
   const slide = (offset) => {
-    const slider = document.getElementById('slider')
+    const slider = document.getElementById('slider' + rowId)
     slider.scrollLeft = slider?.scrollLeft + offset
   }
   return (
@@ -19,7 +20,7 @@ const MovieRow = ({ title, url }: { title: string, url: string }) => {
       <h2 className='font-nsans-bold md:text-xl p-4 capitalize'>{title}</h2>
       <div className='relative flex items-center group'>
         <MdChevronLeft onClick={() => { slide(-500) }} size={40} className='bg-white rounded-full absolute left-2 opacity-80 text-gray-700 z-10 hidden group-hover:block cursor-pointer' />
-        <div id={`slider`} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
+        <div id={`slider` + rowId} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
           {movies.map((movie) => (
             <MovieItem key={movie.id} movie={movie} />
           ))}
